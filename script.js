2ae4ebd560337548ecfbec81107f5f4954fa78c2
PiaoSowanna <chayanon.sowanna@gmail.com>
2025-05-31 18:42:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var app = angular.module('anomalist', []);
 
 app.directive('topMenu', function() {
    return {
-      templateUrl: '/menu.html',
+      templateUrl: '/webpage/menu.html',
       scope: {
         active: '@'
       }
@@ -11,47 +11,47 @@ app.directive('topMenu', function() {
 
 app.directive('carousel', function() {
    return {
-      templateUrl: '/carousel.html'
+      templateUrl: '/webpage/carousel.html'
    };
 });
 
 app.controller('FrontPageCarousel', function($scope){
   $scope.slides = [
     {
-      imageL: '/img/res01a.jpg',
-      imageP: '/img/res01as.jpg'
+      imageL: '/webpage/img/res01a.jpg',
+      imageP: '/webpage/img/res01as.jpg'
     },
     {
-      imageL: '/img/res02a.jpg',
-      imageP: '/img/res02as.jpg'
+      imageL: '/webpage/img/res02a.jpg',
+      imageP: '/webpage/img/res02as.jpg'
     },
     {
-      imageL: '/img/com01a.jpg',
-      imageP: '/img/com01as.jpg'
+      imageL: '/webpage/img/com01a.jpg',
+      imageP: '/webpage/img/com01as.jpg'
     },
     {
-      imageL: '/img/com02a.jpg',
-      imageP: '/img/com02as.jpg'
+      imageL: '/webpage/img/com02a.jpg',
+      imageP: '/webpage/img/com02as.jpg'
     },
     {
-      imageL: '/img/hos01a.jpg',
-      imageP: '/img/hos01as.jpg'
+      imageL: '/webpage/img/hos01a.jpg',
+      imageP: '/webpage/img/hos01as.jpg'
     },
     {
-      imageL: '/img/hos02a.jpg',
-      imageP: '/img/hos02as.jpg'
+      imageL: '/webpage/img/hos02a.jpg',
+      imageP: '/webpage/img/hos02as.jpg'
     },	
     {
-      imageL: '/img/edu01a.jpg',
-      imageP: '/img/edu01as.jpg'
+      imageL: '/webpage/img/edu01a.jpg',
+      imageP: '/webpage/img/edu01as.jpg'
     },
     {
-      imageL: '/img/edu02a.jpg',
-      imageP: '/img/edu02as.jpg'
+      imageL: '/webpage/img/edu02a.jpg',
+      imageP: '/webpage/img/edu02as.jpg'
     },
 	{
-      imageL: '/img/ind01a.jpg',
-      imageP: '/img/ind01as.jpg'
+      imageL: '/webpage/img/ind01a.jpg',
+      imageP: '/webpage/img/ind01as.jpg'
     }	
   ];
 });
@@ -59,20 +59,20 @@ app.controller('FrontPageCarousel', function($scope){
 app.controller('MuseumCarousel', function($scope){
   $scope.slides = [
     {
-      imageP: '/img/homeP.jpg',
-      imageL: '/img/homeL.jpg'
+      imageP: '/webpage/img/homeP.jpg',
+      imageL: '/webpage/img/homeL.jpg'
     },
     {
-      imageP: '/img/homeP.jpg',
-      imageL: '/img/homeL.jpg'
+      imageP: '/webpage/img/homeP.jpg',
+      imageL: '/webpage/img/homeL.jpg'
     },
     {
-      imageP: '/img/homeP.jpg',
-      imageL: '/img/homeL.jpg'
+      imageP: '/webpage/img/homeP.jpg',
+      imageL: '/webpage/img/homeL.jpg'
     },
     {
-      imageP: '/img/homeP.jpg',
-      imageL: '/img/homeL.jpg'
+      imageP: '/webpage/img/homeP.jpg',
+      imageL: '/webpage/img/homeL.jpg'
     }
   ];
 });
@@ -80,32 +80,32 @@ app.controller('MuseumCarousel', function($scope){
 app.controller('BoligCarousel', function($scope){
   $scope.slides = [
     {
-      imageP: '/category/bolig/img/b01.jpg',
-      imageL: '/category/bolig/img/b01W.jpg'
+      imageP: '/webpage/category/bolig/img/b01.jpg',
+      imageL: '/webpage/category/bolig/img/b01W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b02.jpg',
-      imageL: '/category/bolig/img/b02W.jpg'
+      imageP: '/webpage/category/bolig/img/b02.jpg',
+      imageL: '/webpage/category/bolig/img/b02W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b03.jpg',
-      imageL: '/category/bolig/img/b03W.jpg'
+      imageP: '/webpage/category/bolig/img/b03.jpg',
+      imageL: '/webpage/category/bolig/img/b03W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b04.jpg',
-      imageL: '/category/bolig/img/b04W.jpg'
+      imageP: '/webpage/category/bolig/img/b04.jpg',
+      imageL: '/webpage/category/bolig/img/b04W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b05.jpg',
-      imageL: '/category/bolig/img/b05W.jpg'
+      imageP: '/webpage/category/bolig/img/b05.jpg',
+      imageL: '/webpage/category/bolig/img/b05W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b06.jpg',
-      imageL: '/category/bolig/img/b06W.jpg'
+      imageP: '/webpage/category/bolig/img/b06.jpg',
+      imageL: '/webpage/category/bolig/img/b06W.jpg'
     },
     {
-      imageP: '/category/bolig/img/b07.jpg',
-      imageL: '/category/bolig/img/b07W.jpg'
+      imageP: '/webpage/category/bolig/img/b07.jpg',
+      imageL: '/webpage/category/bolig/img/b07W.jpg'
     }	
   ];
 });
