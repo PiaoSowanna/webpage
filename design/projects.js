9755f56d63a591e04a5f3ccb5d1d384c76fceafe
PiaoSowanna <chayanon.sowanna@gmail.com>
2025-06-01 22:10:54 +0200

    Add files via upload

--- design/projects.js
+++ design/projects.js
@@ -7,329 +7,347 @@ var projects = angular.module('projects', []);
 
 projects.controller('pro', function($scope){
   $scope.currentType = undefined;
-  $scope.types = ['Commercial','Hospitality','Educational','Residential'];
+  $scope.types = ['Commercial','Educational','Hospitality','Residential'];
   $scope.projects = [
     {
       image: 'img/commercial/img06b.png',
-      caption: 'Commercial',
+      caption: 'C.01.CAMP',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img07b.png',
-      caption: 'Commercial',
+      caption: 'C.01.CAMP',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img08b.png',
-      caption: 'Commercial',
+      caption: 'C.02',
+      type: 'Commercial',
+	    text: ''
+    },
+    {
+      image: 'img/commercial/img09b.png',
+      caption: 'C.02',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img24b.png',
-      caption: 'Commercial',
+      caption: 'C.03',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img25b.png',
-      caption: 'Commercial',
+      caption: 'C.03',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img26b.png',
-      caption: 'Commercial',
+      caption: 'C.04',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img28b.png',
-      caption: 'Commercial',
+      caption: 'C.04',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img29b.png',
-      caption: 'Commercial',
+      caption: 'C.05',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img30b.png',
-      caption: 'Commercial',
+      caption: 'C.06.WONDER PLANET',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img31b.png',
-      caption: 'Commercial',
+      caption: 'C.06.WONDER PLANET',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img32b.png',
-      caption: 'Commercial',
+      caption: 'C.06.WONDER PLANET',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img33b.png',
-      caption: 'Commercial',
+      caption: 'C.06.WONDER PLANET',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img34b.png',
-      caption: 'Commercial',
+      caption: 'C.06.WONDER PLANET',
       type: 'Commercial',
 	    text: ''
     },
     {
       image: 'img/commercial/img35b.png',
-      caption: 'Commercial',
+      caption: 'C.07',
       type: 'Commercial',
 	    text: ''
     },
     {
-      image: 'img/educational/img2b.png',
-      caption: 'Educational',
-      type: 'Educational',
-	    text: ''
-    } ,
-    {
       image: 'img/educational/img45b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img46b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     },
     {
       image: 'img/educational/img47b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	  text: ''
     } ,
     {
       image: 'img/educational/img48b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img49b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img50b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img51b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
-      image: 'img/educational/img59b.png',
-      caption: 'Educational',
+      image: 'img/educational/img52b.png',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
-    },
+    } ,
     {
       image: 'img/educational/img54b.png',
-      caption: 'Educational',
+      caption: 'E.01.ECO VILLAGE',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img55b.png',
-      caption: 'Educational',
+      caption: 'E.02.FINNSKOGEN',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img56b.png',
-      caption: 'Educational',
+      caption: 'E.02.FINNSKOGEN',
       type: 'Educational',
 	    text: ''
     } ,
     {
       image: 'img/educational/img57b.png',
-      caption: 'Educational',
+      caption: 'E.02.FINNSKOGEN',
       type: 'Educational',
 	    text: ''
     },
     {
       image: 'img/educational/img58b.png',
-      caption: 'Educational',
+      caption: 'E.02.FINNSKOGEN',
+      type: 'Educational',
+	    text: ''
+    },
+    {
+      image: 'img/educational/img59b.png',
+      caption: 'E.02.FINNSKOGEN',
       type: 'Educational',
 	    text: ''
     },
     {
       image: 'img/hospitality/img36b.jpg',
-      caption: 'Hospitality',
+      caption: 'H.01',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img37b.png',
-      caption: 'Hospitality',
+      caption: 'H.01',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img38b.png',
-      caption: 'Hospitality',
+      caption: 'H.01',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img39b.png',
-      caption: 'Hospitality',
+      caption: 'H.01',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img40b.png',
-      caption: 'Hospitality',
+      caption: 'H.01',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img41b.png',
-      caption: 'Hospitality',
+      caption: 'H.02',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img42b.png',
-      caption: 'Hospitality',
+      caption: 'H.02',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img43b.png',
-      caption: 'Hospitality',
+      caption: 'H.02',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/hospitality/img44b.png',
-      caption: 'Hospitality',
+      caption: 'H.02',
       type: 'Hospitality',
 	    text: ''
     },
     {
       image: 'img/residential/img01b.png',
-      caption: 'Residential',
+      caption: 'R.01',
+      type: 'Residential',
+	    text: ''
+    },
+    {
+      image: 'img/residential/img02b.png',
+      caption: 'R.01',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img03b.png',
-      caption: 'Residential',
+      caption: 'R.01',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img04b.png',
-      caption: 'Residential',
+      caption: 'R.01',
+      type: 'Residential',
+	    text: ''
+    },
+    {
+      image: 'img/residential/img05b.png',
+      caption: 'R.01',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img10b.png',
-      caption: 'Residential',
+      caption: 'R.01',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img11b.png',
-      caption: 'Residential',
+      caption: 'R.01',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img12b.jpg',
-      caption: 'Residential',
+      caption: 'R.02',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img13b.jpg',
-      caption: 'Residential',
+      caption: 'R.02',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img14b.jpg',
-      caption: 'Residential',
+      caption: 'R.02',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img15b.png',
-      caption: 'Residential',
+      caption: 'R.03',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img16b.png',
-      caption: 'Residential',
+      caption: 'R.03',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img17b.png',
-      caption: 'Residential',
+      caption: 'R.03',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img18b.png',
-      caption: 'Residential',
+      caption: 'R.03',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img19b.png',
-      caption: 'Residential',
+      caption: 'R.03',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img20b.png',
-      caption: 'Residential',
+      caption: 'R.04',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img21b.png',
-      caption: 'Residential',
+      caption: 'R.04',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img22b.png',
-      caption: 'Residential',
+      caption: 'R.04',
       type: 'Residential',
 	    text: ''
     },
     {
       image: 'img/residential/img23b.png',
-      caption: 'Residential',
+      caption: 'R.04',
       type: 'Residential',
 	    text: ''
     }                                            
