cd23ed370d90efcdb139907a54c14ef239063d83
PiaoSowanna <chayanon.sowanna@gmail.com>
2025-06-01 15:03:10 +0200

    Add files via upload

--- projects/projects.js
+++ projects/projects.js
@@ -7,122 +7,98 @@ var projects = angular.module('projects', []);
 
 projects.controller('pro', function($scope){
   $scope.currentType = undefined;
-  $scope.types = ['Residential','Hospitality','Commercial','Educational','Industrial'];
+  $scope.types = ['Commercial','Hospitality','Commercial','Educational','Industrial'];
   $scope.projects = [
     {
-      image: 'img/res1.jpg',
-      caption: 'Residential',
-      type: 'Residential',
-	  text: ''
-    },
-    {
-      image: 'img/res2.jpg',
-      caption: 'Residential',
-      type: 'Residential',
-	  text: ''
-	  },
-    {
-      image: 'img/res3.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-    {
-      image: 'img/res4.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-    {
-      image: 'img/res5.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-    {
-      image: 'img/res6.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-    {
-      image: 'img/res7.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-	{
-	  image: 'img/res8.jpg',
-	  caption: 'Residential',
-      type: 'Residential',
-	  text: ''    
-	},
-	{
-      image: 'img/comer1.jpg',
+      image: 'img/commercial/img06b.png',
       caption: 'Commercial',
       type: 'Commercial',
 	  text: ''
     },
     {
-      image: 'img/comer2.jpg',
+      image: 'img/commercial/img07b.png',
       caption: 'Commercial',
       type: 'Commercial',
 	  text: ''
     },
-	{
-      image: 'img/comer3.jpg',
+    {
+      image: 'img/commercial/img08b.png',
       caption: 'Commercial',
       type: 'Commercial',
 	  text: ''
-	},
-	{
-      image: 'img/hos1.jpg',
-      caption: 'Hospitality',
-      type: 'Hospitality',
+    },
+    {
+      image: 'img/commercial/img24b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
 	  text: ''
     },
     {
-      image: 'img/hos2.jpg',
-      caption: 'Hospitality',
-      type: 'Hospitality',
-	  text: ''
-    },
-	{
-      image: 'img/hos3.jpg',
-      caption: 'Hospitality',
-      type: 'Hospitality',
-	  text: ''
-	},
-	{
-      image: 'img/edu1.jpg',
-      caption: 'Hospitality',
-      type: 'Hospitality',
-	  text: ''
-	},
-	{
-      image: 'img/edu2.jpg',
-      caption: 'Educational',
-      type: 'Educational',
+      image: 'img/commercial/img25b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
 	  text: ''
     },
     {
-      image: 'img/edu3.jpg',
-      caption: 'Educational',
-      type: 'Educational',
+      image: 'img/commercial/img26b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
 	  text: ''
     },
-	{
-      image: 'img/edu5.jpg',
-      caption: 'Educational',
-      type: 'Educational',
+    {
+      image: 'img/commercial/img27b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
 	  text: ''
-	},
-	{
-      image: 'img/ind1.jpg',
-      caption: 'Industrial',
-      type: 'Industrial',
+    },
+    {
+      image: 'img/commercial/img28b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
 	  text: ''
-	}	
+    },
+    {
+      image: 'img/commercial/img29b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img30b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img31b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img32b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img33b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img34b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    },
+    {
+      image: 'img/commercial/img35b.png',
+      caption: 'Commercial',
+      type: 'Commercial',
+	  text: ''
+    }                                                   
   ];
 });
 
